f13723abe35eddc46f809aef37fb7ddda345928d
David Ordone <david9108@hotmail.com>
2023-05-10 09:30:23 -0300

    Modulo cors para consumir a API no front-end

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@ import express from "express"
 import { initDatabase } from "./config/database.js"
 import routes from "./routes/index.js"
 import * as dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config()
 
 initDatabase()
 
 const app = express()
-app.use(express.json())
+app.use(cors())
 
 routes(app)
 
