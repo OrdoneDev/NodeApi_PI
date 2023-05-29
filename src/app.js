75695da9a27c3945af37cd6c04c2ac5c45b338e4
David Ordone <david9108@hotmail.com>
2023-05-29 13:58:41 -0300

    .

--- src/app.js
+++ src/app.js
@@ -12,7 +12,8 @@ const app = express()
 
 app.use((_, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE')
+    res.header("Access-Control-Allow-Methods", 'GET,OPTIONS,PUT,POST,DELETE')
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
     app.use(cors())
     next()
 })
