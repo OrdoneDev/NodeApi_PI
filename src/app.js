603f586aca656ea0bd363a54cbfbc7d9f98725cf
David Ordone <david9108@hotmail.com>
2023-05-29 13:52:13 -0300

    .

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ const app = express()
 
 app.use((_, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE')
     app.use(cors())
     next()
 })
